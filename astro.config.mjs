fe106f59e75589d119123bafb5075d644666ed19
offlaneDefender <116073804+offlaneDefender@users.noreply.github.com>
2024-03-08 17:40:20 +0200

    Added content to homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://offlanedefender.github.io",
-  base: "/mikko"
+  base: "/mikko",
+  prefetch: true
 });
